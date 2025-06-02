3bdf10c40fc37a932beb5bddb8773c274a7b9138
VijiSubra <viji.subramanyan@yahoo.com>
2025-06-02 10:33:33 -0400

    SAS Token for Blob

--- AzureBlobProject/Services/BlobService.cs
+++ AzureBlobProject/Services/BlobService.cs
@@ -1,5 +1,7 @@
 ﻿using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
 using AzureBlobProject.Models;
 
 namespace AzureBlobProject.Services
@@ -78,6 +80,24 @@ namespace AzureBlobProject.Services
                 {
                     Uri = blobClient.Uri.AbsoluteUri
                 };
+
+                if(blobClient.CanGenerateSasUri)
+                {
+                    BlobSasBuilder blobSasBuilder = new()
+                    {
+                        BlobContainerName = blobClient.GetParentBlobContainerClient().Name,
+                        BlobName = blobClient.Name,
+                        Resource = "b",
+                        ExpiresOn = DateTimeOffset.UtcNow.AddHours(1)
+                    };
+
+                    //blobSasBuilder.Resource = "b";
+
+                    blobSasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+                    blobModel.Uri = blobClient.GenerateSasUri(blobSasBuilder).AbsoluteUri;
+                }
+
                 BlobProperties properties = await blobClient.GetPropertiesAsync();
                 if(properties.Metadata.ContainsKey("title"))
                 {
